9e6726b883d5b2f4b225b0c7cd1a6599fb91638d
Pietro Carta <pietro.carta88@gmail.com>
2019-08-24 13:32:31 +0100

    test merge

--- Assets/Scripts/SimpleCameraController.cs
+++ Assets/Scripts/SimpleCameraController.cs
@@ -12,6 +12,7 @@ namespace UnityTemplateProjects
             public float x;
             public float y;
             public float z;
+            public float daigan;
 
             public void SetFromTransform(Transform t)
             {
