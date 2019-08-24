b638b2f570e9f2480f903d4bf1e09da3b370fee6
Pietro Carta <pietro.carta88@gmail.com>
2019-08-24 14:01:55 +0100

    base system

---
+++ Assets/Scripts/System/StandaloneSystem.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CGJ.System
+{
+    public class StandaloneSystem : SystemManager
+    {
+        public GameObject playerInterface;
+
+        private void Start()
+        {
+            instance = this;
+            StartCoroutine(InitSystem());
+        }
+
+        private IEnumerator InitSystem()
+        {
+            // init stuff in ISystems interface
+            yield return null;
+        }
+    }
+}
---
+++ Assets/Scripts/System/SystemManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+
+namespace CGJ.System
+{
+    public class SystemManager : MonoBehaviour
+    {
+        public static SystemManager instance { get; protected set; }
+        public static ISystems systems { get; protected set; }
+    }
+}
