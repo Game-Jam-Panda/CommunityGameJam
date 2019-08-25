fafcd7079e76b9291d6a5dc3a7223ac3242be2b1
HadiLePanda <hadizorkot@gmail.com>
2019-08-25 11:25:55 +0000

    Added basic player controller and camera follow
    -Added first map and set it up
    -Option to lock/unlock depth movement (locked by default)

---
+++ Assets/Scripts/Camera/CameraFollow.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGJ.Camera
+{
+    public class CameraFollow : MonoBehaviour
+    {
+        [Header("Target")]
+        [SerializeField] GameObject target = null;
+
+        [Header("Camera Movement")]
+        [SerializeField] float followSpeed = 5.0f;
+
+        [Header("Camera Positioning")]
+        [SerializeField] float ZOffset = 10.0f;
+        [SerializeField] float heightOffset = 2.0f;
+
+        float camPosX = 0.0f;
+        float camPosY = 0.0f;
+        float camPosZ = 0.0f;
+
+        void Start()
+        {
+            if(target == null) { return; }
+            
+            // TP the camera to the starting position
+            Vector3 startPosition = new Vector3(target.transform.position.x, heightOffset, -ZOffset);
+            transform.position = startPosition;
+        }
+
+        void Update()
+        {
+            if(target == null) { return; }
+
+            FollowTarget();
+        }
+
+        void FollowTarget()
+        {
+            Vector3 currentPosition = transform.position;
+            Vector3 targetPos = target.transform.position;
+
+            camPosX = targetPos.x;
+            camPosY = heightOffset;      // Height offset
+            camPosZ = -ZOffset;          // Side-view offset
+
+            Vector3 newPosition = new Vector3(camPosX, camPosY, camPosZ);
+
+            // Move the camera towards the target
+            transform.position = Vector3.Lerp(currentPosition, newPosition, followSpeed * Time.deltaTime);
+        }
+    }
+}
---
+++ Assets/Scripts/Control/CharacterMovement.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGJ.Movement
+{
+    public class CharacterMovement : MonoBehaviour
+    {
+        [Header("Movement settings")]
+        [SerializeField] bool lockVerticalMovement = true;
+        [SerializeField] float moveSpeed = 5.0f;
+        [SerializeField] float allowMovementTreshhold = 0.0f;
+
+        //Input
+        float InputX = 0.0f;
+        float InputZ = 0.0f;
+        float inputMagnitude;
+
+        //Movement
+        Vector3 movementDirection;
+        Vector3 movement;
+
+        void Update()
+        {
+            ProcessInput();
+            ProcessMovement();
+        }
+
+        void ProcessInput()
+        {
+            // Left and right movement
+            InputX = Input.GetAxis("Horizontal");
+            
+            // Depth movement
+            if(lockVerticalMovement) { InputZ = 0.0f; return; }
+            InputZ = Input.GetAxis("Vertical");
+        }
+
+        void ProcessMovement()
+        {
+            // Get input magnitude
+            inputMagnitude = new Vector2(InputX, InputZ).sqrMagnitude;
+            var inputMagnitudeClamped = Mathf.Clamp01(inputMagnitude);
+
+            // Move if the magnitude has reached the allowed movement treshhold
+            if (inputMagnitudeClamped > allowMovementTreshhold)
+            {
+                Move();
+            }
+        }
+
+        void Move()
+        {
+            Vector3 charForward = transform.forward;
+            Vector3 charRight = transform.right;
+
+            charForward.y = 0.0f;
+            charRight.y = 0.0f;
+            charForward.Normalize();
+            charRight.Normalize();
+            
+            //Direction to move
+            movementDirection = charForward * InputZ + charRight * InputX;
+            movement = movementDirection * moveSpeed * Time.deltaTime;
+            
+            //Physically move the character
+            transform.Translate(movement);
+        }
+    }
+
+}
