900eb451af3175f8672c9a962e3fc957aaf3dc39
HadiLePanda <hadizorkot@gmail.com>
2019-08-28 10:17:30 +0000

    Main menu & Scene loading
    -Main menu scene
    -Play & Exit buttons
    -GameManager to handle calls for important managers such as SceneManager
    -SceneManager to go to loading screen and load the desired scene
    -New version for leaves falling

---
+++ Assets/Scripts/Core/CGJGameManager.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGJ.Core
+{
+    public class CGJGameManager : MonoBehaviour
+    {
+        CGJSceneManager sceneManager;
+
+        void Awake()
+        {
+            //Always carry the game manager in every scene
+            DontDestroyOnLoad(this);
+
+            //Get references to managers attached to the game manager
+            sceneManager = GetComponent<CGJSceneManager>();
+        }
+
+        public void QuitGame()
+        {
+            #if UNITY_EDITOR
+                UnityEditor.EditorApplication.isPlaying = false;
+            #endif
+            
+            Application.Quit();
+        }
+    }
+
+}
