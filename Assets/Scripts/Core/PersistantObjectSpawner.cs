0122005b2f382dfa18621d844f06f65b3a3c95a7
HadiLePanda <hadizorkot@gmail.com>
2019-08-28 00:51:41 +0000

    Added Main menu start
    -Added main menu
    -Added Game manager & PersistantObjects
    -Added PersistantObjectsSpawner script

---
+++ Assets/Scripts/Core/PersistantObjectSpawner.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CGJ.Core
+{
+	public class PersistantObjectSpawner : MonoBehaviour
+	{
+		[SerializeField] GameObject persistantObjectPrefab = null;
+
+		static bool hasSpawned = false;
+
+		void Awake()
+		{
+            if (hasSpawned) { return; }
+
+            SpawnPersistantObject();
+            hasSpawned = true;
+		}
+
+		void SpawnPersistantObject()
+		{
+			GameObject persistantObject = Instantiate(persistantObjectPrefab);
+			DontDestroyOnLoad(persistantObject);
+		}
+	}
+}
