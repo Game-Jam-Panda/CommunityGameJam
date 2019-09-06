25523fdafabcf46ed3504d5e16dbd114403cf47e
HadiLePanda <hadizorkot@gmail.com>
2019-09-06 00:24:23 +0000

    fake command trap, +
    -Added fake heal command that instant kills, more discrete
    -Tried adding slippery physics on player, but doesn't work with the current movement system, has to use rigidbody to work instead of translation

---
+++ Assets/Scripts/Characters/PlayerCommands.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using CGJ.Core;
+using UnityEngine;
+
+namespace CGJ.Characters
+{
+    public class PlayerCommands : MonoBehaviour
+    {
+        HealthSystem playerHealth = null;
+        
+        void Awake()
+        {
+            playerHealth = GetComponent<HealthSystem>();
+        }
+
+        void Update()
+        {
+            ProcessFakeCommands();
+        }
+
+        void ProcessFakeCommands()
+        {
+            // Instant kill for trying to heal ourselves
+            if(Input.GetKeyDown(KeyCode.H))
+            {
+                playerHealth.TakeDamage(playerHealth.GetCurrentHealth());
+            }
+        }
+    }
+}
